e14105e13eb5eb84f0c425e2234128c0a4a64f42
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-19 14:50:33 +0200

    Declare functions and write case statements

---
+++ Bank.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+    double balance = 0;
+    int choice = 0;
+
+    do {
+        cout << "************************" << endl;
+        cout << "Enter your choice " << endl;
+        cout << "************************" << endl;
+
+        cout << "1. Show Balance" << endl;
+        cout << "2. Deposit" << endl;
+        cout << "3. Withdraw" << endl;
+        cout << "4. exit" << endl;
+        cin >> choice;
+
+        cin.clear();
+        fflush(stdin);
+
+        switch(choice){
+            case 1:
+                showBalance(balance);
+                break;
+            case 2:
+                balance += deposit();
+                showBalance(balance);
+                break;
+            case 3:
+                balance -= withdraw(balance);
+                showBalance(balance);
+                break;
+            case 4:
+                cout << "Thanks for visiting" << endl;
+                break;
+            default:
+                cout << "Invalid choice" << endl;
+        }
+    }while(choice != 4);
+    
+}
+
